ed33c48db11d3f84dd82304c5a3842f159f1c240
Rui Jiang <2031161851@qq.com>
2026-06-09 03:45:20 +0800

    运动控制变量共享

---
+++ Task/app_config.h
@@ -0,0 +1,11 @@
+﻿#ifndef __APP_CONFIG_H
+#define __APP_CONFIG_H
+
+/* ---- 共享运动控制常量 ---- */
+#define STEPS_PER_MM     3276.8f
+#define JOG_MAX_STEPS    8388607
+#define X1_ADDR          0x01
+#define X2_ADDR          0x02
+#define Y_ADDR           0x03
+
+#endif
